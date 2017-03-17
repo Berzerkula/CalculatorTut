bb91939baa31fea42ba51d9f729a66994b0aa057
William Harrington <kb0iic@gmail.com>
2017-03-17 14:42:39 -0500

    Add project files.

---
+++ CalculatorTut/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace CalculatorTut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.equals = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.period = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.mantissa = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // equals
+            // 
+            this.equals.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.equals.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equals.Location = new System.Drawing.Point(36, 263);
+            this.equals.Name = "equals";
+            this.equals.Size = new System.Drawing.Size(271, 35);
+            this.equals.TabIndex = 0;
+            this.equals.Text = "=";
+            this.equals.UseVisualStyleBackColor = false;
+            this.equals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // seven
+            // 
+            this.seven.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.seven.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seven.Location = new System.Drawing.Point(36, 73);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(52, 29);
+            this.seven.TabIndex = 2;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = false;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eight
+            // 
+            this.eight.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.eight.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eight.Location = new System.Drawing.Point(103, 73);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(52, 29);
+            this.eight.TabIndex = 3;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = false;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.nine.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nine.Location = new System.Drawing.Point(175, 73);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(52, 29);
+            this.nine.TabIndex = 4;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = false;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // divide
+            // 
+            this.divide.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.divide.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divide.Location = new System.Drawing.Point(262, 73);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(45, 29);
+            this.divide.TabIndex = 5;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = false;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.multiply.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiply.Location = new System.Drawing.Point(262, 123);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(45, 29);
+            this.multiply.TabIndex = 9;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = false;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // six
+            // 
+            this.six.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.six.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.six.Location = new System.Drawing.Point(175, 123);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(52, 29);
+            this.six.TabIndex = 8;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = false;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // five
+            // 
+            this.five.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.five.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.five.Location = new System.Drawing.Point(103, 123);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(52, 29);
+            this.five.TabIndex = 7;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = false;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // four
+            // 
+            this.four.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.four.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.four.Location = new System.Drawing.Point(36, 123);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(52, 29);
+            this.four.TabIndex = 6;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = false;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.add.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(262, 172);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(45, 29);
+            this.add.TabIndex = 13;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // three
+            // 
+            this.three.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.three.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.three.Location = new System.Drawing.Point(175, 172);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(52, 29);
+            this.three.TabIndex = 12;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = false;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // two
+            // 
+            this.two.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.two.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.two.Location = new System.Drawing.Point(103, 172);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(52, 29);
+            this.two.TabIndex = 11;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = false;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // one
+            // 
+            this.one.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.one.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.one.Location = new System.Drawing.Point(36, 172);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(52, 29);
+            this.one.TabIndex = 10;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = false;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.subtract.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtract.Location = new System.Drawing.Point(262, 221);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(45, 29);
+            this.subtract.TabIndex = 17;
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = false;
+            this.subtract.Click += new System.EventHandler(this.subtract_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.clear.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(175, 221);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(52, 29);
+            this.clear.TabIndex = 16;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // period
+            // 
+            this.period.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.period.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.period.Location = new System.Drawing.Point(103, 221);
+            this.period.Name = "period";
+            this.period.Size = new System.Drawing.Size(52, 29);
+            this.period.TabIndex = 15;
+            this.period.Text = ".";
+            this.period.UseVisualStyleBackColor = false;
+            this.period.Click += new System.EventHandler(this.period_Click);
+            // 
+            // zero
+            // 
+            this.zero.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.zero.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zero.Location = new System.Drawing.Point(36, 221);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(52, 29);
+            this.zero.TabIndex = 14;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = false;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // mantissa
+            // 
+            this.mantissa.BackColor = System.Drawing.Color.Green;
+            this.mantissa.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mantissa.ForeColor = System.Drawing.Color.LightGreen;
+            this.mantissa.Location = new System.Drawing.Point(36, 12);
+            this.mantissa.Name = "mantissa";
+            this.mantissa.ReadOnly = true;
+            this.mantissa.Size = new System.Drawing.Size(271, 44);
+            this.mantissa.TabIndex = 18;
+            this.mantissa.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.mantissa.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(345, 312);
+            this.Controls.Add(this.mantissa);
+            this.Controls.Add(this.subtract);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.period);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.equals);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Calculator 0.1a";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button period;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.TextBox mantissa;
+    }
+}
+
---
+++ CalculatorTut/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculatorTut
+{
+    public partial class Form1 : Form
+    {
+        string input = string.Empty;
+        string operand1 = string.Empty;
+        string operand2 = string.Empty;
+        char operation;
+        double result = 0.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "0";
+            this.mantissa.Text += input;
+        }
+
+        private void period_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += ".";
+            this.mantissa.Text += input;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            this.input = string.Empty;
+            this.operand1 = string.Empty;
+            this.operand2 = string.Empty;
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "1";
+            this.mantissa.Text += input;
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "2";
+            this.mantissa.Text += input;
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "3";
+            this.mantissa.Text += input;
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "4";
+            this.mantissa.Text += input;
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "5";
+            this.mantissa.Text += input;
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "6";
+            this.mantissa.Text += input;
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "7";
+            this.mantissa.Text += input;
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "8";
+            this.mantissa.Text += input;
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            this.mantissa.Text = "";
+            input += "9";
+            this.mantissa.Text += input;
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            operand1 = input;
+            operation = '/';
+            input = string.Empty;
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            operand1 = input;
+            operation = '*';
+            input = string.Empty;
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            operand1 = input;
+            operation = '+';
+            input = string.Empty;
+        }
+
+        private void subtract_Click(object sender, EventArgs e)
+        {
+            operand1 = input;
+            operation = '-';
+            input = string.Empty;
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            operand2 = input;
+            double num1, num2;
+            double.TryParse(operand1, out num1);
+            double.TryParse(operand2, out num2);
+
+            if(operation == '+')
+            {
+                result = num1 + num2;
+                mantissa.Text = result.ToString();
+            }
+            else if(operation == '-')
+            {
+                result = num1 - num2;
+                mantissa.Text = result.ToString();
+            }
+            else if(operation == '*')
+            {
+                result = num1 * num2;
+                mantissa.Text = result.ToString();
+            }
+            else if(operation == '/')
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    mantissa.Text = result.ToString();
+                }
+                else
+                {
+                    mantissa.Text = "DIV/Zero!";
+                }
+            }
+            this.input = string.Empty;
+        }
+    }
+}
